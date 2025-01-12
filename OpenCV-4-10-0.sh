d2d03d94122395ebf27f8d00fc548c872e588d40
Q-engineering <info@qengineering.eu>
2025-01-12 10:39:21 +0100

    Update OpenCV-4-10-0.sh

--- OpenCV-4-10-0.sh
+++ OpenCV-4-10-0.sh
@@ -39,11 +39,25 @@ sudo apt-get install -y liblapack-dev gfortran libhdf5-dev
 sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
 sudo apt-get install -y protobuf-compiler
 
-# download the latest version
+# remove old versions or previous builds
 cd ~ 
 sudo rm -rf opencv*
-git clone --depth=1 https://github.com/opencv/opencv.git
-git clone --depth=1 https://github.com/opencv/opencv_contrib.git
+
+# download the 4.10.0 version
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.10.0.zip 
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.10.0.zip 
+
+# unpack
+unzip opencv.zip 
+unzip opencv_contrib.zip 
+
+# Some administration to make life easier later on
+mv opencv-4.10.0 opencv
+mv opencv_contrib-4.10.0 opencv_contrib
+
+# clean up the zip files
+rm opencv.zip
+rm opencv_contrib.zip
 
 # set install dir
 cd ~/opencv
